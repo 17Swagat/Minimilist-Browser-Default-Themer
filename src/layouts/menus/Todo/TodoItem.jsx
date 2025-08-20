8b88be12218d68260260928c6359d3d86444a84c
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-08-21 02:51:06 +0530

    Also put a delete icon in todoitem delete button

--- src/layouts/menus/Todo/TodoItem.jsx
+++ src/layouts/menus/Todo/TodoItem.jsx
@@ -27,15 +27,6 @@ export default function TodoItem({ todo_item }) {
   return (
     <div className="flex items-center bg-gray-300 rounded-lg shadow-md p-2 hover:shadow-lg transition duration-200">
 
-      {/* V1 */}
-      {/* <input
-        type="text"
-        className="flex-1 p-3 text-gray-800 bg-gray-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 overflow-y-auto"
-        value={todoContent}
-        readOnly={!isEditable}
-        onChange={(e) => setTodoContent(e.target.value)}
-      /> */}
-
       <input
         ref={inputRef}
         type="text"
@@ -66,11 +57,11 @@ export default function TodoItem({ todo_item }) {
 
       </button>
       <button
-        className="ml-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300 active:scale-95 select-none font-medium"
+        className="ml-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition duration-300 active:scale-95 select-none font-medium"
         onClick={() => {
           deleteTodo(todo_item.id)
         }}>
-        Delete
+        🗑️
       </button>
     </div>
   );
