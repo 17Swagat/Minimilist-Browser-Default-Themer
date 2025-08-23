56d431161764ebb02ca9dc1b86fae065e17e973a
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-08-23 16:36:14 +0530

    Diving Google Apps Menu into component

--- src/layouts/ModalMenus.jsx
+++ src/layouts/ModalMenus.jsx
@@ -3,17 +3,20 @@ import { GMenu } from "./menus/GApps/GMenu";
 import { useAppStateContext } from "../contexts/AppStates";
 import AddWebShortcutMenu from "./menus/AddWebShortcut/AddWebShortcut";
 
-import gicon_search from "../assets/icons/google_apps/google.png";
-import gicon_mail from "../assets/icons/google_apps/gmail.png";
-import gicon_forms from "../assets/icons/google_apps/google-forms.png";
-import gicon_keep from "../assets/icons/google_apps/keeps.png";
-import gicon_youtube from "../assets/icons/google_apps/youtube.png";
-import gicon_maps from "../assets/icons/google_apps/maps.png";
+// import gicon_search from "../assets/icons/google_apps/google.png";
+// import gicon_mail from "../assets/icons/google_apps/gmail.png";
+// import gicon_forms from "../assets/icons/google_apps/google-forms.png";
+// import gicon_keep from "../assets/icons/google_apps/keeps.png";
+// import gicon_youtube from "../assets/icons/google_apps/youtube.png";
+// import gicon_maps from "../assets/icons/google_apps/maps.png";
+// import gicon_ytmusic from "../assets/icons/google_apps/yt_music.png";
+import GAppsIcons from "../assets/icons/google_apps";
 
 
 // Menu Importing
 import TodoMenuUI from "./menus/Todo/TodoMenu";
 import SettingsMenuUI from "./menus/Settings/SettingsMenuUI";
+import { GMenu_Body } from "./menus/GApps/GMenuBody";
 
 export default function ModalMenus() {
   const {
@@ -79,82 +82,61 @@ export default function ModalMenus() {
         toggleMenuStateFunc: toggleGoogleAppsMenu,
         menuName: "Google Apps",
         menuBody: googleAppsMenuOpen && (
-          <div className="w-full h-full grid grid-cols-3 sm:grid-cols-4 gap-4 p-4
-          ">
-            {[
-
-              { name: "Search", icon: gicon_search },
-              { name: "Gmail", icon: gicon_mail },
-              { name: "Keep", icon: gicon_keep },
-              { name: "Forms", icon: gicon_forms },
-              { name: "Maps", icon: gicon_maps },
-              { name: "YouTube", icon: gicon_youtube },
-            ].map((app) => (
-              <div
-                key={app.name}
-                className="flex flex-col justify-center items-center cursor-pointer hover:bg-white/10 p-3 rounded-lg transition-all duration-200"
-                onClick={() => window.open(app.url, "_blank")}
-              >
-                <img
-                  src={app.icon}
-                  alt={`${app.name} icon`}
-                  className="w-14 h-14 mb-2"
-                />
-                <span className="text-white text-sm font-medium text-center">
-                  {app.name}
-                </span>
-              </div>
-            ))}
-          </div>
+          <GMenu_Body/>
+          // <div className="w-full h-full grid grid-cols-3 sm:grid-cols-4 gap-4 p-4
+          // ">
+          //   {
+          //     [
+          //       { name: "Search", icon: GAppsIcons.gsearch, url: "https://www.google.com" },
+          //       { name: "Gemini", icon: GAppsIcons.gemini, url: "https://gemini.google.com/app" },
+          //       { name: "Gmail", icon: GAppsIcons.gmail, url: "https://mail.google.com" },
+          //       { name: "Drive", icon: GAppsIcons.drive, url: "https://drive.google.com" },
+          //       { name: "Youtube", icon: GAppsIcons.youtube, url: "https://www.youtube.com" },
+          //       { name: "YTMusic", icon: GAppsIcons.ytmusic, url: "https://music.youtube.com" },
+          //       { name: "Keep", icon: GAppsIcons.keeps, url: "https://keep.google.com" },
+          //       { name: "Forms", icon: GAppsIcons.forms, url: "https://docs.google.com/forms" },
+          //       { name: "Slides", icon: GAppsIcons.slides, url: "https://docs.google.com/presentation" },
+          //       { name: "Tasks", icon: GAppsIcons.tasks, url: "https://tasks.google.com" },
+          //       { name: "Maps", icon: GAppsIcons.maps, url: "https://www.google.com/maps" },
+          //       { name: "Calendar", icon: GAppsIcons.calendar, url: "https://calendar.google.com" },
+          //       { name: "Earth", icon: GAppsIcons.earth, url: "https://earth.google.com" },
+          //       { name: "Books", icon: GAppsIcons.books, url: "https://books.google.com" },
+          //       { name: "Docs", icon: GAppsIcons.docs, url: "https://docs.google.com/document" },
+          //       { name: "Finance", icon: GAppsIcons.finance, url: "https://finance.google.com" },
+          //       { name: "Forms", icon: GAppsIcons.forms, url: "https://docs.google.com/forms" },
+          //       { name: "Keeps", icon: GAppsIcons.keeps, url: "https://keep.google.com" },
+          //       { name: "Maps", icon: GAppsIcons.maps, url: "https://www.google.com/maps" },
+          //       { name: "Meet", icon: GAppsIcons.meet, url: "https://meet.google.com" },
+          //       { name: "News", icon: GAppsIcons.news, url: "https://news.google.com" },
+          //       { name: "Password", icon: GAppsIcons.password, url: "https://passwords.google.com" },
+          //       { name: "Photos", icon: GAppsIcons.photos, url: "https://photos.google.com" },
+          //       { name: "Scholar", icon: GAppsIcons.scholar, url: "https://scholar.google.com" },
+          //       { name: "Sheets", icon: GAppsIcons.sheets, url: "https://docs.google.com/spreadsheets" },
+          //       { name: "Shopping", icon: GAppsIcons.shopping, url: "https://shopping.google.com" },
+          //       { name: "Sites", icon: GAppsIcons.sites, url: "https://sites.google.com" },
+          //       { name: "Translate", icon: GAppsIcons.translate, url: "https://translate.google.com" },
+          //       { name: "Webstore", icon: GAppsIcons.webstore, url: "https://chrome.google.com/webstore" },
+          //       { name: "Travel", icon: GAppsIcons.travel, url: "https://www.google.com/travel" },
+          //       { name: "Blogger", icon: GAppsIcons.blogger, url: "https://www.blogger.com" }
+          //     ].map((app) => (
+          //       <div
+          //         key={app.name}
+          //         className="flex flex-col justify-center items-center cursor-pointer hover:bg-white/10 p-3 rounded-lg transition-all duration-200"
+          //         onClick={() => window.open(app.url, "_blank")}
+          //       >
+          //         <img
+          //           src={app.icon}
+          //           alt={`${app.name} icon`}
+          //           className="w-14 h-14 mb-2"
+          //         />
+          //         <span className="text-white text-center">
+          //           {app.name}
+          //         </span>
+          //       </div>
+          //     ))}
+          // </div>
         )
       })}
-
-      {/* ************************ */}
-      {/* #2 */}
-      {/* {GMenu(
-        googleAppsMenuOpen,
-        toggleGoogleAppsMenu,
-        "Google Apps",
-        googleAppsMenuOpen && (
-          <div className="w-full h-full bg-red-400 flex flex-wrap overflow-y-auto">
-            {[
-              { name: "Search", icon: gicon_search },
-              { name: "Gmail", icon: gicon_mail },
-              { name: "Keep", icon: gicon_keep },
-              { name: "Forms", icon: gicon_forms },
-              { name: "Maps", icon: gicon_maps },
-              { name: "YouTube", icon: gicon_youtube },
-            ].map((app) => (
-              <div
-                key={app.name}
-                className="w-1/4 flex flex-col justify-center items-center cursor-pointer hover:bg-white/10 p-2 rounded-lg transition"
-                onClick={() => {
-                  if (app.name.toLowerCase() != "youtube") {
-                    window.open(
-                      `https://${app.name.toLowerCase()}.google.com`,
-                      "_blank"
-                    );
-                  } else {
-                    window.open(
-                      `https://${app.name.toLowerCase()}.com`,
-                      "_blank"
-                    );
-                  }
-                }}
-              >
-                <img
-                  src={app.icon}
-                  alt={`${app.name} icon`}
-                  className="w-12 h-12 mb-2"
-                />
-                <span className="text-white text-sm text-center">
-                  {app.name}
-                </span>
-              </div>
-            ))}
-          </div>
-        )
-      )} */}
 
 
       {/* Add WebShortcut Shortcut Menu */}
--- src/layouts/menus/GApps/GMenu.jsx
+++ src/layouts/menus/GApps/GMenu.jsx
@@ -8,7 +8,7 @@ export function GMenu({ menuState, toggleMenuStateFunc, menuName, menuBody }) {
     <>
       {modalWindowOpen && menuState && (
         <div
-          className="w-[800px] max-w-[90vw] h-[600px] max-h-[80vh] bg-gradient-to-br from-[#5c3facff] to-[#3b2a7bff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl shadow-2xl flex flex-col z-50
+          className="w-[900px] max-w-[90vw] h-[800px] max-h-[80vh] bg-gradient-to-br from-[#5c3facff] to-[#3b2a7bff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl shadow-2xl flex flex-col z-50
           custom-scrollbar scrollbar-consistent
           "
           onClick={(e) => e.stopPropagation()}
@@ -36,61 +36,3 @@ export function GMenu({ menuState, toggleMenuStateFunc, menuName, menuBody }) {
     </>
   );
 }
-
-
-// Old:
-// import { useAppStateContext } from "../../../contexts/AppStates";
-
-// export function GMenu(
-//   menuState,
-//   toggleMenuStateFunc,
-//   menuName,
-//   menuBody
-// ) {
-
-//   const {modalWindowOpen, toggleModalWindow, closeAllMenus } = useAppStateContext()
-
-//   return (
-//     <>
-//       {
-//         // isModalOpen_state && (
-//         (modalWindowOpen && menuState) && (
-//           <div
-//             // className="w-1/2 h-1/2 bg-[#5c3facff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg flex flex-col justify-center items-center z-15"
-//             className="w-[700px] h-[500px] bg-[#5c3facff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg flex flex-col justify-center items-center z-15"
-//             onClick={(e) => {
-//               e.stopPropagation(); // Prevent click from closing the modal
-//             }}>
-            
-//             {/* Menu Heading */}
-//             <div className="flex items-center h-[15%]">
-//               <h1 className="text-white text-2xl tracking-wide">{menuName}</h1>
-              
-//               {/* Close Button */}
-//               <div className="absolute right-3.5">
-//                 <div
-//                   className=" bg-blue-500 p-3.5 rounded-full h-[30px] w-[30px] 
-//                   flex justify-center items-center 
-//                   text-white text-2xl font-bold 
-//                   transition duration-300 ease active:bg-red-700"
-//                   onClick={(e) => {
-//                     e.stopPropagation();
-//                     closeAllMenus()
-//                     // toggleMenuStateFunc(!menuState)
-//                   }}>
-               
-//                   X
-//                 </div>
-
-//               </div>
-
-//             </div>
-
-//             {/* Menu Content */}
-//             <div className="w-full h-[85%] bg-green-600">{menuBody}</div>
-//           </div>
-//         )
-//       }
-//     </>
-//   );
-// }
